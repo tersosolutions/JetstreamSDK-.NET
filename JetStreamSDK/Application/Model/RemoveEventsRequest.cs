2d8a65b4f57c8d83e56556ed77d7b18cbbb07f37
Mark Neustadt <mark.neustadt@tersosolutions.com>
2013-06-14 13:11:30 -0500

    Added Get/Remove Events

---
+++ JetStreamSDK/Application/Model/RemoveEventsRequest.cs
@@ -0,0 +1,55 @@
+﻿﻿/*
+     Copyright 2013 Terso Solutions
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+using System;
+using System.Web;
+
+namespace TersoSolutions.Jetstream.Application.Model
+{
+    public class RemoveEventsRequest : JetstreamRequest
+    {
+        private const String c_removeeventscommand = "v1.2/application/?action=removeevents&accesskey={0}&batchid={1}";
+
+        public string BatchId { get; set; }
+
+        internal override string BuildUri(string baseUri, string accesskey)
+        {
+            // build the uri
+            return String.Concat(baseUri, String.Format(c_removeeventscommand, 
+                accesskey, HttpUtility.UrlEncode(this.BatchId)));
+        }
+    }
+}
+
+///// <summary>
+///// ResetCommand request object
+///// </summary>
+///// <remarks>Author Mike Lohmeier</remarks>
+//public class ResetCommandRequest : JetstreamRequest
+//{
+//    private const String c_resetCommand = "v1.0/application/?action=resetcommand&accesskey={0}&logicaldeviceid={1}";
+
+//    /// <summary>
+//    /// The LogicalDeviceId to schedule a reset command for
+//    /// </summary>
+//    public String LogicalDeviceId { get; set; }
+
+//    internal override string BuildUri(string baseUri, string accesskey)
+//    {
+//        // build the uri
+//        return String.Concat(baseUri, String.Format(c_resetCommand,
+//            accesskey, HttpUtility.UrlEncode(this.LogicalDeviceId)));
+//    }
+//}
---
+++ JetStreamSDK/Application/Model/RemoveEventsResponse.cs
@@ -0,0 +1,48 @@
+﻿﻿/*
+     Copyright 2012 Terso Solutions
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+using System;
+using CR = TersoSolutions.Jetstream.Application.Model.Deserialized.CommandResponse;
+
+namespace TersoSolutions.Jetstream.Application.Model
+{
+    /// <summary>
+    /// Response object for the Jetstream version 1.2 RemoveEvents endpoint
+    /// </summary>
+    /// <remarks>Author Mark Bailey</remarks>
+    public class RemoveEventsResponse : JetstreamResponse
+    {
+        private CR.Jetstream _deserializedResponse = null;
+
+        /// <summary>
+        /// The access key the device should use for access to the Jetstream device endpoints
+        /// </summary>
+        public String CommandId
+        {
+            get
+            {
+                if (!String.IsNullOrEmpty(this.Body))
+                {
+                    _deserializedResponse = _deserializedResponse ?? CR.Jetstream.Deserialize(this.Body);
+                    return _deserializedResponse.CommandResponse.CommandId;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
