a6b9bdf63a14a2c1af41029ab32bb7f6f37f2813
Mark Neustadt <mark.neustadt@tersosolutions.com>
2014-03-05 08:53:48 -0600

    S43-030514A Update
    Updated to remove RemoveEventsById, to revise RemoveEvents and to
    correct documentation URLs in ServiceClient

--- JetStreamSDK/Application/Model/RemoveEventsRequest.cs
+++ JetStreamSDK/Application/Model/RemoveEventsRequest.cs
@@ -15,25 +15,59 @@
  */
 using System;
 using System.Web;
+using System.Linq;
 
 namespace TersoSolutions.Jetstream.SDK.Application.Model
 {
     /// <summary>
     /// RemoveEvents removes event messages associated with a batch of events from the user’s queue.
     /// 
-    /// Request object for Jetstream version 1.2 RemoveEvents endpoint.
+    /// Request object for Jetstream version 1.3 RemoveEvents endpoint.
     /// </summary>
     public class RemoveEventsRequest : JetstreamRequest
     {
-        private const String c_removeeventscommand = "v1.2/application/?action=removeevents&accesskey={0}&batchid={1}";
+        private const String c_removeeventscommand = "v1.3/application/?action=removeevents&accesskey={0}{1}{2}";
 
+        /// <summary>
+        /// The batch id of the events to remove
+        /// </summary>
         public string BatchId { get; set; }
+
+        /// <summary>
+        /// An array of event ids to remove
+        /// </summary>
+        public string[] EventIds { get; set; }
 
         internal override string BuildUri(string baseUri, string accesskey)
         {
+            // assume no batch id nor events
+            string usingBatch = String.Empty;
+            string usingEvents = String.Empty;
+
+            // if the user has passed a batch id, build that portion of the query string request
+            if (!String.IsNullOrEmpty(BatchId))
+            {
+                usingBatch = String.Format("&batchid={0}", HttpUtility.UrlEncode(BatchId));
+            }
+
+            // if the user has passed event ids, build that portion of the query string request
+            if (EventIds.Count() != 0)
+            {
+                // URLEncode each event id
+                System.Collections.Generic.List<string> list = new System.Collections.Generic.List<string>();
+                foreach (var ev in EventIds)
+                {
+                    list.Add(HttpUtility.UrlEncode(ev));
+                }
+
+                // Build the event ids string
+                string idString = String.Join("_", list.ToArray());
+                usingEvents = String.Format("&eventids={0}", idString);
+            }
+
             // build the uri
-            return String.Concat(baseUri, String.Format(c_removeeventscommand, 
-                accesskey, HttpUtility.UrlEncode(this.BatchId)));
+            return String.Concat(baseUri, String.Format(c_removeeventscommand, accesskey, usingBatch, usingEvents));
+
         }
     }
 }
